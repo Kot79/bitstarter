07d1dafc7501597aebb3dbeccd7188f750c03584
Ubuntu <ubuntu@ip-172-31-10-127.us-west-2.compute.internal>
2013-09-11 14:48:53 +0000

    web.js was modified

--- web.js
+++ web.js
@@ -1,8 +1,11 @@
 var express = require('express');
+var fs = require('fs');
+var htmlfile = "index.html";
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
+  var html = fs.readFileSync(htmlfile).toString();
   response.sendfile('index.html');
 });
 
